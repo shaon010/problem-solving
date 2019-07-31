0e8fbb99a4db603ba471b1e6d38f25440ed893e6
Md. Amir Hossain <amir.hossain@bjitgroup.com>
2019-07-31 11:32:34 +0600

    leetCode - Longest Substring Without Repeating Characters - 3 - Accepted - Bruitforce

--- src/leetcode/longestSubstringNoRepeat_3/Solution.java
+++ src/leetcode/longestSubstringNoRepeat_3/Solution.java
@@ -3,20 +3,22 @@ package leetcode.longestSubstringNoRepeat_3;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * Accepted solution
+ * BruitForce
+ * Runtime: 200 ms -_-
+ */
 class Solution {
+    int maxSize = 0;
+
     public int lengthOfLongestSubstring(String s) {
-        int maxSize = 0;
-
-        return maxSize;
-    }
-
-    int findSubStringLength(String s) {
-        int maxSize = 0;
         int currentSize = 0;
         List<Character> charList = new ArrayList<>();
         for (char c: s.toCharArray()) {
             if (charList.contains(c)) {
-                return currentSize;
+                maxSize =  currentSize > maxSize ? currentSize : maxSize;
+                lengthOfLongestSubstring(s.substring(s.indexOf(c) + 1));
+                break;
             }
             else {
                 charList.add(c);
@@ -28,6 +30,12 @@ class Solution {
     }
 
     public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new Solution().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new Solution().lengthOfLongestSubstring("pwwkew"));
+        System.out.println(new Solution().lengthOfLongestSubstring(""));
+        System.out.println(new Solution().lengthOfLongestSubstring(" "));
         System.out.println(new Solution().lengthOfLongestSubstring("dvdf"));
+        System.out.println(new Solution().lengthOfLongestSubstring("babcdb"));
     }
 }
