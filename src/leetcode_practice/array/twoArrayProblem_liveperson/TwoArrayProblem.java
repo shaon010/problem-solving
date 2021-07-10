ed4a33f7a71a16bc50abf7a0bdcde9fff74c98ec
al.monsur <al.monsur@bjitgroup.com>
2021-07-10 17:18:02 +0600

    Two array problem live person with single for loop
    (cherry picked from commit c6fa000498aea4ea0ac2e7e185a032323da0c138)

---
+++ src/leetcode_practice/array/twoArrayProblem_liveperson/TwoArrayProblem.java
@@ -0,0 +1,27 @@
+package leetcode.array.twoArrayProblem_liveperson;
+
+import java.util.*;
+
+//given two array list. Find out the numbers from first array which are available at most once at 2nd array.
+//[1, 2, 3] , [4, 5, 1, 6, 7, 3, 9, 3] => [1, 2]
+
+public class TwoArrayProblem {
+    public List<Integer> twoArray(List<Integer> list1, List<Integer> list2) {
+        List<Integer> matchedDataList = new ArrayList<>();
+        List<Integer> result = new ArrayList<>(list1);
+        list2.forEach(number -> {
+            if (list1.contains(number)) {
+                if (matchedDataList.contains(number)) {
+                    result.remove(number);
+                }
+                matchedDataList.add(number);
+            }
+        });
+        return result;
+    }
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(new Integer[]{1, 2, 3});
+        List<Integer> list2 = Arrays.asList(new Integer[]{4, 5, 1, 6, 7, 3, 9, 3});
+        System.out.println(new TwoArrayProblem().twoArray(list1, list2));
+    }
+}
