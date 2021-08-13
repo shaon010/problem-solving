fee8c9f8243b3accbc0fc2c64ac686c1a533d3ca
Shaon <shaon010@gmail.com>
2021-08-14 01:00:04 +0600

    Reverse Array with two pointer

--- src/leetcode_tecognize/array/chapterOne/ArrayReverseTwoPointer.java
+++ src/leetcode_tecognize/array/chapterOne/ArrayReverseTwoPointer.java
@@ -1,5 +1,22 @@
 package leetcode_tecognize.array.chapterOne;
 
 public class ArrayReverseTwoPointer {
-
+    public char[] reverseArray(char[] charArray) {
+        int length = charArray.length;
+        int j = length - 1;
+        for (int i = 0; i < j; i++) {
+            char iChar = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = iChar;
+            j--;
+        }
+        return charArray;
+    }
+    public static void main(String[] args) {
+        ArrayReverseTwoPointer arrayReverseTwoPointer = new ArrayReverseTwoPointer();
+        String input = "Hello";
+        String input2 = "Amir";
+        System.out.println(arrayReverseTwoPointer.reverseArray(input.toCharArray())); // olleH
+        System.out.println(arrayReverseTwoPointer.reverseArray(input2.toCharArray())); // rimA
+    }
 }
