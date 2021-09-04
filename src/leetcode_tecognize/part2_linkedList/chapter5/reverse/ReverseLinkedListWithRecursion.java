ddf85ad711e8e810e85add6c39006ee8b51766ef
os-amir.hossain <os-amir.hossain@rakute.com>
2021-09-04 18:56:02 +0600

    linkedList reverse with recursion

--- src/leetcode_tecognize/part2_linkedList/chapter5/reverse/ReverseLinkedListWithRecursion.java
+++ src/leetcode_tecognize/part2_linkedList/chapter5/reverse/ReverseLinkedListWithRecursion.java
@@ -4,16 +4,23 @@ import leetcode_tecognize.part2_linkedList.chapter4.implementation.LinkedList;
 
 import java.util.Arrays;
 
-//todo implement
 //recursive: https://www.geeksforgeeks.org/recursively-reversing-a-linked-list-a-simple-implementation/
-//without recursion, with a loop only: https://www.geeksforgeeks.org/reverse-a-linked-list/
 public class ReverseLinkedListWithRecursion {
+    LinkedList.Node reverse(LinkedList.Node node) {
+        if (node.next == null) {
+            return node;
+        }
+        LinkedList.Node newHead = reverse(node.next); // all recursive value will return the last node that we get at last
+        node.next.next = node;
+        node.next = null;
+        return newHead;
+    }
 
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
         Arrays.stream(new int[]{1, 2, 3, 4}).forEach(linkedList::insert);
         ReverseLinkedListWithRecursion reverse = new ReverseLinkedListWithRecursion();
-        //reverse.reverse(linkedList.head);
-        System.out.println(linkedList);
+        LinkedList.Node newHead = reverse.reverse(linkedList.head);
+        System.out.println(newHead);
     }
 }
